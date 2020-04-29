086374479e497d7787f1f53c70973b03f2bb8abe
rajas <rajashekarraju123@gmail.com>
2020-04-29 23:57:23 +0530

    Removed authentication usages from ViewModels

--- app/src/main/java/com/developersbreach/loginandroid/list/ListViewModel.kt
+++ app/src/main/java/com/developersbreach/loginandroid/list/ListViewModel.kt
@@ -1,32 +1,6 @@
 package com.developersbreach.loginandroid.list
 
-import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
-import com.developersbreach.loginandroid.AuthenticationState
 
-class ListViewModel : ViewModel() {
 
-    var authenticationState = MutableLiveData<AuthenticationState>()
-
-    init {
-        authenticationState.value = AuthenticationState.UNAUTHENTICATED
-    }
-
-    fun verifyUser(username: String?, password: String?) {
-        if (validateUserAccount(username, password)) {
-            authenticationState.value = AuthenticationState.AUTHENTICATED
-        } else {
-            authenticationState.value = AuthenticationState.UNAUTHENTICATED
-        }
-    }
-
-    private fun validateUserAccount(username: String?, password: String?): Boolean {
-        var result = false
-
-        if (username != null && password != null) {
-            result = username.length >= 5 && password.length >= 5
-        }
-
-        return result
-    }
-}
+class ListViewModel : ViewModel()
--- app/src/main/java/com/developersbreach/loginandroid/login/LoginViewModel.kt
+++ app/src/main/java/com/developersbreach/loginandroid/login/LoginViewModel.kt
@@ -1,34 +1,6 @@
 package com.developersbreach.loginandroid.login
 
-import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
-import com.developersbreach.loginandroid.AuthenticationState
 
-class LoginViewModel : ViewModel() {
 
-    var authenticationState = MutableLiveData<AuthenticationState>()
-
-    init {
-        authenticationState.value = AuthenticationState.UNAUTHENTICATED
-    }
-
-    fun authenticate(username: String, password: String) {
-        if (validateUserAccount(username, password)) {
-            authenticationState.value = AuthenticationState.AUTHENTICATED
-        } else {
-            authenticationState.value = AuthenticationState.UNAUTHENTICATED
-        }
-    }
-
-    private fun validateUserAccount(username: String, password: String): Boolean {
-        return if (username.isNotEmpty() && password.isNotEmpty()) {
-            username.length >= 5 && password.length >= 5
-        } else {
-            false
-        }
-    }
-
-    fun skippedAuthentication() {
-        authenticationState.value = AuthenticationState.UNAUTHENTICATED
-    }
-}
+class LoginViewModel : ViewModel()

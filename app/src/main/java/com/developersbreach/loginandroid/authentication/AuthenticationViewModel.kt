4ad5cf76d7e2e4a19bacbeb111b8dd74242ff139
rajas <rajashekarraju123@gmail.com>
2020-04-30 23:58:57 +0530

    Moved ViewModel logic to SharedViewModel

--- app/src/main/java/com/developersbreach/loginandroid/authentication/AuthenticationViewModel.kt
+++ app/src/main/java/com/developersbreach/loginandroid/authentication/AuthenticationViewModel.kt
@@ -1,33 +1,6 @@
 package com.developersbreach.loginandroid.authentication
 
-import androidx.lifecycle.MutableLiveData
+
 import androidx.lifecycle.ViewModel
 
-class AuthenticationViewModel : ViewModel() {
-
-    val authenticationState = MutableLiveData<AuthenticationState>()
-
-    init {
-        authenticationState.value = AuthenticationState.UNAUTHENTICATED
-    }
-
-    fun verifyUser(username: String?, password: String?) {
-        if (validateUserAccount(username, password)) {
-            authenticationState.value = AuthenticationState.AUTHENTICATED
-        } else {
-            authenticationState.value = AuthenticationState.UNAUTHENTICATED
-        }
-    }
-
-    private fun validateUserAccount(username: String?, password: String?): Boolean {
-        var result = false
-        if (username != null && password != null) {
-            result = username.length >= 5 && password.length >= 5
-        }
-        return result
-    }
-
-    fun unAuthenticateUser() {
-        authenticationState.value = AuthenticationState.UNAUTHENTICATED
-    }
-}
+class AuthenticationViewModel : ViewModel()

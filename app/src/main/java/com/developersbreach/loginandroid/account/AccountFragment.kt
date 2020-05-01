f7643378e4917d6a79bc7b7ebfc39ea1b56b419f
rajas <rajashekarraju123@gmail.com>
2020-05-01 23:59:37 +0530

    Fixed back and up navigation button behaviours

--- app/src/main/java/com/developersbreach/loginandroid/account/AccountFragment.kt
+++ app/src/main/java/com/developersbreach/loginandroid/account/AccountFragment.kt
@@ -1,15 +1,19 @@
 package com.developersbreach.loginandroid.account
 
 import android.os.Bundle
+import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.Button
 import android.widget.TextView
+import android.widget.Toast
+import androidx.activity.OnBackPressedCallback
 import androidx.fragment.app.Fragment
 import androidx.lifecycle.Observer
 import androidx.lifecycle.ViewModelProvider
 import androidx.navigation.Navigation
+import androidx.navigation.fragment.findNavController
 import com.developersbreach.loginandroid.R
 import com.developersbreach.loginandroid.authentication.AuthenticationState
 import com.developersbreach.loginandroid.authentication.PrefUtils
@@ -18,15 +22,17 @@ import com.google.android.material.snackbar.Snackbar
 
 class AccountFragment : Fragment() {
 
-    private val viewModel: AccountViewModel by lazy {
-        ViewModelProvider(this).get(AccountViewModel::class.java)
-    }
+    private lateinit var viewModel: AccountViewModel
 
     private var username: String? = String()
     private var password: String? = String()
     private lateinit var userTextView: TextView
     private lateinit var logoutButton: Button
 
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        viewModel = ViewModelProvider(this).get(AccountViewModel::class.java)
+    }
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
@@ -39,10 +45,29 @@ class AccountFragment : Fragment() {
         return view
     }
 
-    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
-        super.onViewCreated(view, savedInstanceState)
+    override fun onActivityCreated(savedInstanceState: Bundle?) {
+        super.onActivityCreated(savedInstanceState)
+        viewModel.authenticationState.observe(viewLifecycleOwner, observer())
+    }
 
-        viewModel.authenticationState.observe(viewLifecycleOwner, Observer { authenticationState ->
+    private fun handleBackPress() {
+        requireActivity().onBackPressedDispatcher.addCallback(object :
+            OnBackPressedCallback(true) {
+            override fun handleOnBackPressed() {
+                Navigation.findNavController(requireView()).navigate(R.id.listFragment)
+                Toast.makeText(context, "Pressed", Toast.LENGTH_SHORT).show()
+            }
+        })
+    }
+
+    override fun onDestroyView() {
+        super.onDestroyView()
+        viewModel.authenticationState.removeObserver(observer())
+        Log.e("ListFragment", "Destroyed")
+    }
+
+    private fun observer(): Observer<AuthenticationState> {
+        return Observer { authenticationState ->
             if (authenticationState == AuthenticationState.AUTHENTICATED) {
                 userTextView.text = username
                 logoutButton.text = getString(R.string.user_logout_button_text)
@@ -50,20 +75,21 @@ class AccountFragment : Fragment() {
                 logoutButton.setOnClickListener {
                     viewModel.logoutUser()
                     Snackbar.make(
-                        view,
+                        requireView(),
                         getString(R.string.user_logged_out_message),
                         Snackbar.LENGTH_SHORT
                     ).show()
                 }
+                handleBackPress()
 
             } else if (authenticationState == AuthenticationState.UNAUTHENTICATED) {
                 userTextView.text = getString(R.string.user_not_logged_in)
                 logoutButton.text = getString(R.string.user_login_button_text)
 
-                logoutButton.setOnClickListener {
+                logoutButton.setOnClickListener { view ->
                     Navigation.findNavController(view).navigate(R.id.loginFragment)
                 }
             }
-        })
+        }
     }
 }

81890716ad70c2ed22bf6c936c9f99dcb6013a48
rajas <rajashekarraju123@gmail.com>
2020-05-01 23:59:08 +0530

    Preference for verifying user authentication

--- app/src/main/java/com/developersbreach/loginandroid/authentication/PrefUtils.kt
+++ app/src/main/java/com/developersbreach/loginandroid/authentication/PrefUtils.kt
@@ -1,18 +1,35 @@
 package com.developersbreach.loginandroid.authentication
 
+import android.app.Application
 import android.content.Context
 import android.content.SharedPreferences
+import androidx.lifecycle.MutableLiveData
 
 class PrefUtils {
 
     companion object {
 
-        var PREFERENCE_NAME_USER_DETAILS = "pref_key_user"
-        var PREFERENCE_VALUE_USER_USERNAME: String = "username"
-        var PREFERENCE_VALUE_USER_PASSWORD: String = "password"
+        private var PREFERENCE_NAME_USER_DETAILS = "pref_key_user"
+        private var PREFERENCE_VALUE_USER_USERNAME: String = "username"
+        private var PREFERENCE_VALUE_USER_PASSWORD: String = "password"
         var PREFERENCE_VALUE_DEFAULT_USERNAME: String = "User not logged in"
         var PREFERENCE_VALUE_DEFAULT_PASSWORD: String = "User not logged in"
 
+
+        fun verifyAuthentication(
+            application: Application,
+            authenticationState: MutableLiveData<AuthenticationState>
+        ) {
+            val username: String? = getUsernamePrefs(application.applicationContext)
+            val password: String? = getPasswordPrefs(application.applicationContext)
+            if (!username!!.contentEquals(PREFERENCE_VALUE_DEFAULT_USERNAME) &&
+                !password!!.contentEquals(PREFERENCE_VALUE_DEFAULT_PASSWORD)
+            ) {
+                authenticationState.postValue(AuthenticationState.AUTHENTICATED)
+            } else {
+                authenticationState.postValue(AuthenticationState.UNAUTHENTICATED)
+            }
+        }
 
         fun saveToPreferences(username: String, password: String, context: Context) {
             val namePreferences: SharedPreferences = context.getSharedPreferences(
@@ -32,8 +49,8 @@ class PrefUtils {
                 Context.MODE_PRIVATE
             )
             with(namePreferences.edit()) {
-                putString(PREFERENCE_VALUE_USER_USERNAME, "username")
-                putString(PREFERENCE_VALUE_USER_PASSWORD, "password")
+                putString(PREFERENCE_VALUE_USER_USERNAME, PREFERENCE_VALUE_DEFAULT_USERNAME)
+                putString(PREFERENCE_VALUE_USER_PASSWORD, PREFERENCE_VALUE_DEFAULT_PASSWORD)
                 commit()
             }
         }

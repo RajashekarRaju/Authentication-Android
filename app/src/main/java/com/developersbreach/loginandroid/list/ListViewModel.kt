b657877a455099bc1f49997550b180947646aeb2
rajas <rajashekarraju123@gmail.com>
2020-05-02 18:42:26 +0530

    Added new properties to show list in fragment

--- app/src/main/java/com/developersbreach/loginandroid/list/ListViewModel.kt
+++ app/src/main/java/com/developersbreach/loginandroid/list/ListViewModel.kt
@@ -2,9 +2,11 @@ package com.developersbreach.loginandroid.list
 
 import android.app.Application
 import androidx.lifecycle.AndroidViewModel
+import androidx.lifecycle.LiveData
 import androidx.lifecycle.MutableLiveData
 import com.developersbreach.loginandroid.authentication.AuthenticationState
 import com.developersbreach.loginandroid.authentication.PrefUtils
+import com.developersbreach.loginandroid.model.Sports
 import kotlinx.coroutines.CoroutineScope
 import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.Job
@@ -24,11 +26,19 @@ class ListViewModel(application: Application) : AndroidViewModel(application) {
     val username: MutableLiveData<String>
         get() = _username
 
+    private var sports: MutableLiveData<List<Sports>> = MutableLiveData()
+
     init {
         coroutineScope.launch {
             PrefUtils.verifyAuthentication(application, _authenticationState)
             _username.postValue(PrefUtils.getUsernamePrefs(application.applicationContext))
+            val sportsList: List<Sports> = Sports.sportsList(application.applicationContext)
+            sports.value = sportsList
         }
+    }
+
+    fun getSports(): LiveData<List<Sports>> {
+        return sports
     }
 
     override fun onCleared() {

b3c040e7edb3b42d3f3d352c93d9a8ac9a292212
rajas <rajashekarraju123@gmail.com>
2020-05-06 18:49:27 +0530

    Added text fields for user to register

--- app/src/main/java/com/developersbreach/loginandroid/register/RegisterFragment.kt
+++ app/src/main/java/com/developersbreach/loginandroid/register/RegisterFragment.kt
@@ -1,33 +1,76 @@
 package com.developersbreach.loginandroid.register
 
-import androidx.lifecycle.ViewModelProviders
 import android.os.Bundle
-import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-
+import android.widget.Button
+import androidx.fragment.app.Fragment
+import androidx.lifecycle.Observer
+import androidx.lifecycle.ViewModelProvider
+import androidx.navigation.NavDirections
+import androidx.navigation.Navigation
 import com.developersbreach.loginandroid.R
+import com.developersbreach.loginandroid.authentication.AuthenticationState
+import com.google.android.material.textfield.TextInputEditText
+import com.google.android.material.textfield.TextInputLayout
 
 class RegisterFragment : Fragment() {
 
-    companion object {
-        fun newInstance() = RegisterFragment()
-    }
-
     private lateinit var viewModel: RegisterViewModel
+    private lateinit var firstNameInputLayout: TextInputLayout
+    private lateinit var lastNameInputLayout: TextInputLayout
+    private lateinit var mailInputLayout: TextInputLayout
+    private lateinit var countryInputLayout: TextInputLayout
+    private lateinit var passwordInputLayout: TextInputLayout
+    private lateinit var firstNameEditText: TextInputEditText
+    private lateinit var lastNameEditText: TextInputEditText
+    private lateinit var mailEditText: TextInputEditText
+    private lateinit var countryEditText: TextInputEditText
+    private lateinit var passwordEditText: TextInputEditText
+    private lateinit var registerUserButton: Button
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        return inflater.inflate(R.layout.fragment_register, container, false)
+        val view: View = inflater.inflate(R.layout.fragment_register, container, false)
+        firstNameInputLayout = view.findViewById(R.id.first_name_input_layout)
+        lastNameInputLayout = view.findViewById(R.id.last_name_input_layout)
+        mailInputLayout = view.findViewById(R.id.mail_input_layout)
+        countryInputLayout = view.findViewById(R.id.country_input_layout)
+        passwordInputLayout = view.findViewById(R.id.password_input_layout)
+        firstNameEditText = view.findViewById(R.id.first_name_edit_text)
+        lastNameEditText = view.findViewById(R.id.last_name_edit_text)
+        mailEditText = view.findViewById(R.id.mail_edit_text)
+        countryEditText = view.findViewById(R.id.country_edit_text)
+        passwordEditText = view.findViewById(R.id.password_edit_text)
+        registerUserButton = view.findViewById(R.id.sign_up_user)
+        return view
     }
 
     override fun onActivityCreated(savedInstanceState: Bundle?) {
         super.onActivityCreated(savedInstanceState)
-        viewModel = ViewModelProviders.of(this).get(RegisterViewModel::class.java)
-        // TODO: Use the ViewModel
+        viewModel = ViewModelProvider(this).get(RegisterViewModel::class.java)
+        registerUserButton.setOnClickListener {
+            viewModel.validateFields(
+                firstNameEditText.text.toString(), firstNameInputLayout,
+                lastNameEditText.text.toString(), lastNameInputLayout,
+                mailEditText.text.toString(), mailInputLayout,
+                countryEditText.text.toString(), countryInputLayout,
+                passwordEditText.text.toString(), passwordInputLayout
+            )
+        }
+
+        viewModel.authenticationState.observe(viewLifecycleOwner, Observer { authenticationState ->
+            if (authenticationState == AuthenticationState.AUTHENTICATED) {
+                navigateToListFragment(requireView())
+            }
+        })
     }
 
+    private fun navigateToListFragment(view: View) {
+        val action: NavDirections = RegisterFragmentDirections.actionRegisterToListFragment()
+        Navigation.findNavController(view).navigate(action)
+    }
 }
--- app/src/main/java/com/developersbreach/loginandroid/register/RegisterViewModel.kt
+++ app/src/main/java/com/developersbreach/loginandroid/register/RegisterViewModel.kt
@@ -1,7 +1,77 @@
 package com.developersbreach.loginandroid.register
 
-import androidx.lifecycle.ViewModel
+import android.app.Application
+import androidx.lifecycle.AndroidViewModel
+import androidx.lifecycle.MutableLiveData
+import com.developersbreach.loginandroid.R
+import com.developersbreach.loginandroid.authentication.AuthenticationState
+import com.developersbreach.loginandroid.authentication.PrefUtils
+import com.google.android.material.textfield.TextInputLayout
+import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.Job
+import kotlinx.coroutines.launch
 
-class RegisterViewModel : ViewModel() {
+class RegisterViewModel(application: Application) : AndroidViewModel(application) {
 
+    private var mApplication: Application = application
+    private var viewModelJob = Job()
+    private val coroutineScope = CoroutineScope(viewModelJob + Dispatchers.Main)
+
+    private val _authenticationState = MutableLiveData<AuthenticationState>()
+    val authenticationState: MutableLiveData<AuthenticationState>
+        get() = _authenticationState
+
+    init {
+        coroutineScope.launch {
+            PrefUtils.verifyAuthentication(application, _authenticationState)
+        }
+    }
+
+    fun validateFields(
+        firstNameEditText: String,
+        firstNameInputLayout: TextInputLayout,
+        lastNameEditText: String,
+        lastNameInputLayout: TextInputLayout,
+        mailEditText: String,
+        mailInputLayout: TextInputLayout,
+        countryEditText: String,
+        countryInputLayout: TextInputLayout,
+        passwordEditText: String,
+        passwordInputLayout: TextInputLayout
+    ) {
+        if (firstNameEditText.isNotEmpty() && lastNameEditText.isNotEmpty() &&
+            mailEditText.isNotEmpty() && countryEditText.isNotEmpty() && passwordEditText.isNotEmpty()
+        ) {
+            validateRegistrationAndLogin(firstNameEditText, passwordEditText)
+        } else {
+            if (firstNameEditText.isEmpty())
+                firstNameInputLayout.error = mApplication.getString(R.string.requires_field)
+            else firstNameInputLayout.error = null
+            if (lastNameEditText.isEmpty())
+                lastNameInputLayout.error = mApplication.getString(R.string.requires_field)
+            else lastNameInputLayout.error = null
+            if (mailEditText.isEmpty())
+                mailInputLayout.error = mApplication.getString(R.string.requires_field)
+            else mailInputLayout.error = null
+            if (countryEditText.isEmpty())
+                countryInputLayout.error = mApplication.getString(R.string.requires_field)
+            else countryInputLayout.error = null
+            if (passwordEditText.isEmpty())
+                passwordInputLayout.error = mApplication.getString(R.string.requires_field)
+            else passwordInputLayout.error = null
+        }
+    }
+
+    private fun validateRegistrationAndLogin(username: String, password: String) {
+        coroutineScope.launch {
+            PrefUtils.saveToPreferences(username, password, mApplication.applicationContext)
+            _authenticationState.postValue(AuthenticationState.AUTHENTICATED)
+        }
+    }
+
+    override fun onCleared() {
+        super.onCleared()
+        viewModelJob.cancel()
+    }
 }
